ff46a2f9ddb667ce48ff79fa5463eabc75aa3c9d
Robert Komaromi <chipit24@users.noreply.github.com>
2021-05-04 23:02:21 -0400

    Create house with ground

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,6 +11,7 @@ import {
   HemisphericLight,
   MeshBuilder,
   Sound,
+  Tools,
 } from '@babylonjs/core';
 import villageSound from '/village_sound.mp3';
 
@@ -27,8 +28,16 @@ export default {
     new HemisphericLight('light', new Vector3(10, 10, 0), scene);
 
     const box = MeshBuilder.CreateBox('box', {}, scene);
-    box.position.y = 0.5;
-    MeshBuilder.CreateGround('ground', { width: 10, height: 10 });
+    box.position = new Vector3(-5, 2, 5);
+    box.scaling = new Vector3(8, 4, 6);
+    box.rotation = new Vector3(0, Tools.ToRadians(45), 0);
+
+    const roof = MeshBuilder.CreateCylinder('roof', { tessellation: 3 });
+    roof.scaling = new Vector3(2, 4, 7);
+    roof.rotation = new Vector3(0, Tools.ToRadians(45), Tools.ToRadians(90));
+    roof.position = new Vector3(-5, 4.5, 5);
+
+    MeshBuilder.CreateGround('ground', { width: 20, height: 20 });
 
     new Sound('name', villageSound, scene, null, { loop: true, autoplay: true });
 
