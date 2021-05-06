a0af6820beb8497fbc68824e60d8d4bf04f9753e
Robert Komaromi <chipit24@users.noreply.github.com>
2021-05-05 20:41:16 -0400

    Refactor and pull out method for creating a house

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,53 +15,76 @@ import {
   StandardMaterial,
   Color3,
   Texture,
+  TransformNode,
 } from '@babylonjs/core';
 import villageSound from '/village_sound.mp3';
 import roofTexture from '@/textures/roof.jpeg';
 import brickTexture from '@/textures/brick.jpeg';
 
 export default {
+  data() {
+    return {
+      scene: null,
+    };
+  },
   mounted() {
     const canvas = this.$refs.renderCanvas;
     const engine = new Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true });
-    const scene = new Scene(engine);
+    this.scene = new Scene(engine);
 
-    const camera = new ArcRotateCamera('camera', -Math.PI / 2, Math.PI / 2.5, 3, new Vector3(0, 0, 0), scene);
+    const camera = new ArcRotateCamera('camera', -Math.PI / 2, Math.PI / 2.5, 3, new Vector3(0, 0, 0), this.scene);
     camera.attachControl();
     camera.wheelPrecision = 25;
 
-    new HemisphericLight('light', new Vector3(10, 10, 0), scene);
+    new Sound('name', villageSound, this.scene, null, { loop: true, autoplay: true });
 
-    const box = MeshBuilder.CreateBox('box', {}, scene);
-    box.position = new Vector3(-5, 2, 5);
-    box.scaling = new Vector3(8, 4, 6);
-    box.rotation = new Vector3(0, Tools.ToRadians(45), 0);
-    const boxMaterial = new StandardMaterial('boxMaterial', scene);
-    boxMaterial.diffuseTexture = new Texture(brickTexture, scene);
-    box.material = boxMaterial;
-
-    const roof = MeshBuilder.CreateCylinder('roof', { tessellation: 3 });
-    roof.scaling = new Vector3(2, 4, 7);
-    roof.rotation = new Vector3(0, Tools.ToRadians(45), Tools.ToRadians(90));
-    roof.position = new Vector3(-5, 4.5, 5);
-    const roofMaterial = new StandardMaterial('roofMaterial', scene);
-    roofMaterial.diffuseTexture = new Texture(roofTexture, scene);
-    roof.material = roofMaterial;
+    new HemisphericLight('light', new Vector3(10, 10, 0), this.scene);
 
     const ground = MeshBuilder.CreateGround('ground', { width: 20, height: 20 });
-    const groundMaterial = new StandardMaterial('groundMaterial', scene);
+    const groundMaterial = new StandardMaterial('groundMaterial', this.scene);
     groundMaterial.diffuseColor = Color3.FromHexString('#608038');
     ground.material = groundMaterial;
 
-    new Sound('name', villageSound, scene, null, { loop: true, autoplay: true });
-
     engine.runRenderLoop(() => {
-      scene.render();
+      this.scene.render();
     });
 
     window.addEventListener('resize', () => {
       engine.resize();
     });
+
+    this.createHouse();
+  },
+  methods: {
+    createHouse() {
+      const box = MeshBuilder.CreateBox('box', {
+        wrap: true,
+      }, this.scene);
+      box.position = new Vector3(-5, 2, 5);
+      box.scaling = new Vector3(8, 4, 6);
+      box.rotation = new Vector3(0, Tools.ToRadians(45), 0);
+      const boxMaterial = new StandardMaterial('boxMaterial', this.scene);
+      boxMaterial.diffuseTexture = new Texture(brickTexture, this.scene);
+      box.material = boxMaterial;
+
+      const roof = MeshBuilder.CreateCylinder('roof', {
+        tessellation: 3,
+        wrap: true,
+      });
+      roof.scaling = new Vector3(2, 4, 7);
+      roof.rotation = new Vector3(0, Tools.ToRadians(45), Tools.ToRadians(90));
+      roof.position = new Vector3(-5, 4.5, 5);
+      const roofMaterial = new StandardMaterial('roofMaterial', this.scene);
+      roofMaterial.diffuseTexture = new Texture(roofTexture, this.scene);
+      roof.material = roofMaterial;
+
+      const group = new TransformNode('houseGroup');
+      [box, roof].forEach(mesh => {
+        mesh.parent = group;
+      });
+
+      return [box, roof];
+    },
   },
 };
 </script>
